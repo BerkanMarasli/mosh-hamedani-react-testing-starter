966adbea1e3ab74f4230edb51bb12cfd7f0b8560
Berkan Marasli <berkan.marasli@hotmail.com>
2024-03-07 14:46:11 +0000

    Simplified TermsAndConditions test file

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,34 +3,35 @@ import TermsAndConditions from "../../src/components/TermsAndConditions"
 import userEvent from "@testing-library/user-event"
 
 describe("TermsAndConditions component", () => {
-  it("must render with correct test and initial state", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />)
 
-    const heading = screen.getByRole("heading")
-    expect(heading).toBeInTheDocument()
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button", { name: "Submit" }),
+    }
+  }
+
+  it("must render with correct test and initial state", () => {
+    const { heading, checkbox, button } = renderComponent()
+
     expect(heading).toHaveTextContent("Terms & Conditions")
-
-    const checkbox = screen.getByRole("checkbox")
-    expect(checkbox).toBeInTheDocument()
     expect(checkbox).not.toBeChecked()
-
-    const button = screen.getByRole("button", { name: "Submit" })
-    expect(button).toBeInTheDocument()
     expect(button).toHaveTextContent("Submit")
     expect(button).toBeDisabled()
   })
 
   it("must enable the button when the checkbox is checked", async () => {
-    render(<TermsAndConditions />)
-
+    const { checkbox, button } = renderComponent()
+    
     /*
     Simulate user interaction by using @testing-library/user-event
     Prefer this package over fireEvent which is a wrapper around dispatchEvent API (does not simulate truly how users interact)
     */
-    const checkbox = screen.getByRole("checkbox")
     const user = userEvent.setup()
     await user.click(checkbox)
 
-    expect(screen.getByRole("button")).toBeEnabled()
+    expect(button).toBeEnabled()
   })
 })
