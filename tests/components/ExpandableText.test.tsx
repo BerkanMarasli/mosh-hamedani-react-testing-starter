bb5115f1e32625e25ef71701122ad5d6de03784d
Berkan Marasli <berkan.marasli@hotmail.com>
2024-03-07 14:39:20 +0000

    Exercise: test ExpandableText

---
+++ tests/components/ExpandableText.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react"
+import ExpandableText from "../../src/components/ExpandableText"
+import userEvent from "@testing-library/user-event"
+
+describe("ExpandableText component", () => {
+  const limit = 255
+  const longText = "a".repeat(limit + 1)
+  const truncatedText = longText.substring(0, 255) + "..."
+
+  it("must render the full text if less than 255 characters", () => {
+    const shortText = "Some short text"
+    render(<ExpandableText text={shortText} />)
+
+    expect(screen.getByText(shortText)).toBeInTheDocument()
+  })
+
+  it("must render the truncated text if longer than 255 characters", () => {
+    render(<ExpandableText text={longText} />)
+
+    expect(screen.getByText(truncatedText)).toBeInTheDocument()
+
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent('Show More')
+  })
+
+  it("must expand text when Show More button is clicked", async () => {
+    render(<ExpandableText text={longText} />)
+
+    const button = screen.getByRole("button")
+    const user = userEvent.setup()
+    await user.click(button)
+
+    expect(screen.getByText(longText)).toBeInTheDocument()
+    expect(button).toHaveTextContent('Show Less')
+  })
+})
